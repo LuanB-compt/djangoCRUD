2f2a4bfbd8b99a64ee1077cda28b34e4a6f16bc0
LuanB-compt <luan-bruno-domingues@hotmail.com>
2023-07-02 22:50:54 -0300

    Created a route to create in database

--- src/core/views.py
+++ src/core/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Employee
 
-# Create your views here.
+import json
+
+
 def home(request):
     return render(request=request, template_name='index.html')
+
+@csrf_exempt
+def create(request):
+    if request.method!="POST":
+        return HttpResponse(JsonResponse(data={'msg':'não recebido'}), status=500)
+    request_json = json.loads(request.body.decode("utf-8"))
+    Employee.objects.create(
+        name=request_json['name'],
+        age=request_json['age'],
+        cellphone=request_json['cellphone'],
+        position=request_json['position'],
+        salary=request_json['salary']    
+    )
+    return HttpResponse(JsonResponse(data={'msg':'recebido'}), status=200)
+
+def read(request):
+    pass
+
+def update(request):
+    pass
+
+def delete(request):
+    pass
